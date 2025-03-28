d2ee472db7050392de71c9755566f2a682b63709
DanCrise183 <matvey.uvk1@gmail.com>
2025-03-28 15:31:58 +0300

    Домашнее задание 8

--- trainer_ui_swing/src/main/java/ru/dancrise/gui/Main.java
+++ trainer_ui_swing/src/main/java/ru/dancrise/gui/Main.java
@@ -2,26 +2,25 @@ package ru.dancrise.gui;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.dancrise.domain.service.QuestionService;
-import ru.dancrise.gui.config.SpringConfig;
 import ru.dancrise.gui.controller.MainController;
 
 
 import javax.swing.*;
-
-
-public class Main {
+@SpringBootApplication
+public class Main  {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Main.class)
+                .headless(false).run(args);
         logger.info("Начало работы приложения");
-
-        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-
-
-        QuestionService questionService = context.getBean(QuestionService.class);
+        QuestionService questionService = ctx.getBean(QuestionService.class);
         SwingUtilities.invokeLater(new MainController(questionService));
         logger.info("Конец работы приложения");
     }
